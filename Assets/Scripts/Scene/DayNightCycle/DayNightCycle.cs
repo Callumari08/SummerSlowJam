d8fb39abc5d0a8c7f63aa2e26d93c419a4999e6a
Callum Feeney <87500561+Callumari08@users.noreply.github.com>
2023-08-13 23:59:50 +0100

    Time Framework

--- Assets/Scripts/Scene/DayNightCycle/DayNightCycle.cs
+++ Assets/Scripts/Scene/DayNightCycle/DayNightCycle.cs
@@ -1,18 +1,67 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DayNightCycle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent OnDay;
+    public UnityEvent OnNight;
+
+    public GameTime dayTimes;
+    public GameTime nightTimes;
+    public GameTime currentTime;
+    public float secondsBetweenIncrement = 10f;
+
+    [SerializeField] TextMeshProUGUI clock;
+    bool dayLastFrame;
+
+    public struct GameTime
     {
-        
+        public int Day;
+        public int Hour;
+        public int Min;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        // Initialize currentTime here...
+        StartCoroutine(IncrementTime());
+        UpdateClockText();
+    }
+
+    IEnumerator IncrementTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsBetweenIncrement);
+
+            currentTime.Min += 10;
+
+            while (currentTime.Min >= 60)
+            {
+                currentTime.Hour++;
+                currentTime.Min -= 60;
+            }
+
+            while (currentTime.Hour >= 24)
+            {
+                currentTime.Day++;
+                currentTime.Hour -= 24;
+            }
+
+            UpdateClockText();
+            CheckDayNightEvents();
+        }
+    }
+
+    void UpdateClockText()
+    {
+        clock.text = $"Day {currentTime.Day}: {currentTime.Hour:00}:{currentTime.Min:00}";
+    }
+
+    void CheckDayNightEvents()
+    {
+        // Add code to trigger day or night events based on the currentTime here
     }
 }
