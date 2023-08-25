c89a78c8da536eb8c8d9f3ede61906c1def22eb6
Mischa Larkins <MischaLarkins06@gmail.com>
2023-08-25 01:27:11 -0700

    Settings Done!
    It's only volume and fullscreen, I tried to keep it simple.

--- Assets/Scripts/Other/Settings.cs
+++ Assets/Scripts/Other/Settings.cs
@@ -1,11 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour
 {
     public GameObject settingsPanel;
+    public Slider volumeSlider;
+    public AudioMixer mix;
+    bool isFullscreen;
 
+    void Start() { settingsPanel.SetActive(false); }
     public void OpenPanel() { settingsPanel.SetActive(true); }
     public void ClosePanel() { settingsPanel.SetActive(false); }
+
+    public void OnFullToggleChange(bool tickOn)
+    {
+        if (tickOn)
+        {
+            isFullscreen = true;
+            Debug.Log("fullscreen on");
+        }
+        else
+        {
+            isFullscreen = false;
+            Debug.Log("fullscreen off");
+        }
+    }
+
+    void Update()
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void ChangeVolume()
+    {
+        mix.SetFloat("Vol", volumeSlider.value);
+    }
 }
