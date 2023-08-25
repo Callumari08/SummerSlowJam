d68505ecae4a2f563a7a003056db05164c2e913c
Mischa Larkins <MischaLarkins06@gmail.com>
2023-08-25 05:29:53 -0700

    NPC Additions Framework
    added a framework to make it to where NPCs will have random hair and possible glasses.

--- Assets/Scripts/NPC/NPCLogic.cs
+++ Assets/Scripts/NPC/NPCLogic.cs
@@ -8,12 +8,35 @@ public class NPCLogic : MonoBehaviour
     public float budget;
     public bool moneyPaid;
 
+    public List<GameObject> Hairs;
+    public List<GameObject> Glasses;
+
+    public Transform hairPos;
+    public Transform glassPos;
+
     GameObject root;
 
     void Awake()
     {
         root = GameObject.Find("RootSpawn");
         SpawnRoot();
+
+        //Instancing
+        Instantiate(Hairs[Random.Range(0,Hairs.Count)], hairPos.position, hairPos.rotation);
+        if(randomBool())
+        {
+            Instantiate(Glasses[Random.Range(0,Glasses.Count)], glassPos.position, glassPos.rotation);
+        }
+        
+    }
+
+    bool randomBool()
+    {
+        if (Random.value >= 0.5)
+        {
+            return true;
+        }
+        return false;
     }
     
     void Update()
